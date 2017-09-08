b5e6c61729e0c248773393a6c992a786c21c3dd3
Diego Bernal <diego91bernal@gmail.com>
2017-09-08 11:21:20 -0700

    debug site.js

--- _site/js/site.js
+++ _site/js/site.js
@@ -23,6 +23,7 @@ $(document).ready(function() {
   });
 
   function collapseNavbar() {
+    console.log('navbar.offset().top', navbar.offset().top);
     // Collapse the navbar if page position is greater than 100px from the top
     if (navbar.offset().top > 100) {
       navbar.addClass("navbar-shrink");
