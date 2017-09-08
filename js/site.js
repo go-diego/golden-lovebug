3717e11b4e453563b720a3703d3c27b70c4a8150
diegob <diego91bernal@gmail.com>
2017-09-07 21:19:33 -0700

    Made landing page responsive. Added footer

--- js/site.js
+++ js/site.js
@@ -1,38 +1,34 @@
-(function($) {
-  "use strict"; // Start of use strict
+$(document).ready(function() {
+  var navbar = $('nav.navbar.nav-home');
+  var seeMoreBtn = $('.js-scroll-trigger.btn-see-more');
+  $('.footerYear').html(new Date().getFullYear());
 
-  // Smooth scrolling using jQuery easing
-  $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function() {
+  collapseNavbar();
+
+  seeMoreBtn.click(function() {
     if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
       var target = $(this.hash);
       target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
       if (target.length) {
         $('html, body').animate({
-          scrollTop: (target.offset().top - 54)
-        }, 1000, "easeInOutExpo");
+          scrollTop: (target.offset().top)
+        }, 1000, "easeInOutSine");
         return false;
       }
     }
   });
 
-  // Closes responsive menu when a scroll trigger link is clicked
-  $('.js-scroll-trigger').click(function() {
-    $('.navbar-collapse').collapse('hide');
-  });
-
-  // Activate scrollspy to add active class to navbar items on scroll
-  $('body').scrollspy({
-    target: '#mainNav',
-    offset: 54
-  });
-
-  // Collapse the navbar when page is scrolled
   $(window).scroll(function() {
-    if ($("#mainNav").offset().top > 100) {
-      $("#mainNav.nav-home").addClass("navbar-shrink");
-    } else {
-      $("#mainNav.nav-home").removeClass("navbar-shrink");
-    }
+    collapseNavbar();
   });
 
-})(jQuery); // End of use strict
+  function collapseNavbar() {
+    // Collapse the navbar if page position is greater than 100px from the top
+    if (navbar.offset().top > 100) {
+      navbar.addClass("navbar-shrink");
+    } else {
+      navbar.removeClass("navbar-shrink");
+    }
+  }
+
+});
