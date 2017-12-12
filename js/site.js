886045523941a8c55f946ff04927ebfc9e88c3f0
diegob <diego91bernal@gmail.com>
2017-12-11 21:26:05 -0800

    feature: add contact page
    Markup and validation via javascript is there. Need to test against
    Netlify

--- js/site.js
+++ js/site.js
@@ -3,10 +3,14 @@
 // TODO: Set active page by url
 // TODO: Test onload functionality
 
-$(document).ready(function() {
+$(document).ready(function () {
   //var preloader = $(".preloader");
   var navbar = $("nav.navbar.nav-home");
   var seeMoreBtn = $(".js-scroll-trigger.btn-see-more");
+  var contactForm = $("#contact-form");
+  var requiredFormFields = contactForm.find('[required="required"]');
+  var formSubmitButton = $("button.btn-submit");
+  var formGroupHasError;
 
   //removeSpinner();
 
@@ -14,17 +18,15 @@ $(document).ready(function() {
 
   collapseNavbar();
 
-  seeMoreBtn.click(function() {
+  seeMoreBtn.click(function () {
     if (
-      location.pathname.replace(/^\//, "") ==
-        this.pathname.replace(/^\//, "") &&
+      location.pathname.replace(/^\//, "") == this.pathname.replace(/^\//, "") &&
       location.hostname == this.hostname
     ) {
       var target = $(this.hash);
       target = target.length ? target : $("[name=" + this.hash.slice(1) + "]");
       if (target.length) {
-        $("html, body").animate(
-          {
+        $("html, body").animate({
             scrollTop: target.offset().top
           },
           1000,
@@ -35,7 +37,7 @@ $(document).ready(function() {
     }
   });
 
-  $(window).scroll(function() {
+  $(window).scroll(function () {
     collapseNavbar();
   });
 
@@ -47,6 +49,44 @@ $(document).ready(function() {
       navbar.removeClass("navbar-shrink");
     }
   }
+
+  contactForm.submit(function (e) {
+    e.preventDefault();
+
+    var $form = $(this);
+    if (isFormValid()) {
+      $.post($form.attr("action"), $form.serialize()).then(function () {
+        alert("Thank you!");
+      });
+    }
+
+  });
+
+  function isFormValid() {
+    formGroupHasError = $('.form-group.has-error');
+    return formGroupHasError.length > 0 ? false : true;
+  }
+
+  requiredFormFields.change(function () {
+    var $this = $(this);
+    var $formGroup = $this.parents('.form-group');
+    if (!$this.val() || $this.val() == "") {
+      $formGroup.addClass('has-error');
+    } else {
+      $formGroup.removeClass('has-error');
+    }
+
+    var emptyRequiredFields = requiredFormFields.filter(function () {
+      return $(this).val() == "";
+    });
+
+    if (isFormValid() && emptyRequiredFields.length == 0) {
+      formSubmitButton.removeAttr('disabled');
+    } else {
+      formSubmitButton.attr('disabled', true);
+    }
+  });
+
 
   // function removeSpinner() {
   //   setTimeout(function() {
