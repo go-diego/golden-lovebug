0b5b1f82075c3c57e3edde4a31d78ce9cdba9894
Diego Bernal <diego91bernal@gmail.com>
2018-09-21 23:04:49 -0700

    refactor: move to webpack

---
+++ root/src/site.js
@@ -0,0 +1,98 @@
+require("./scss/site.scss");
+import $ from "jquery";
+window.jQuery = $;
+window.$ = $;
+require("popper.js");
+import "bootstrap";
+
+$(document).ready(() => {
+    var navbar = $("nav.navbar.nav-home");
+
+    collapseNavbar();
+
+    $(window).scroll(function() {
+        collapseNavbar();
+    });
+
+    function collapseNavbar() {
+        if (navbar.length > 0 && navbar.offset().top > 100) {
+            navbar.addClass("navbar-shrink");
+        } else {
+            navbar.removeClass("navbar-shrink");
+        }
+    }
+
+    var $contactForm = $("#contact-form");
+    var $formSubmitButton = $("button.btn-submit");
+    $contactForm.submit(function(e) {
+        e.preventDefault();
+
+        var $form = $(this);
+        if ($form[0].checkValidity()) {
+            var $cardContent = $contactForm.parent();
+            $.post($form.attr("action"), $form.serialize()).then(
+                function success() {
+                    var successAlert = [
+                        '<div class="mt-3 alert alert-success alert-dismissible fade show" role="alert">',
+                        '<button type="button" class="close" data-dismiss="alert" aria-label="Close">',
+                        '<span aria-hidden="true">&times;</span>',
+                        "</button>",
+                        "<strong>Success!</strong> I have received your message and will reply to as soon as possible.",
+                        "</div>"
+                    ]
+                        .join("")
+                        .replace(/\s\s+/g, "");
+
+                    $cardContent.append(successAlert);
+
+                    $contactForm.trigger("reset");
+                },
+                function error() {
+                    var errorAlert = [
+                        '<div class="mt-3 alert alert-danger alert-dismissible fade show" role="alert">',
+                        '<button type="button" class="close" data-dismiss="alert" aria-label="Close">',
+                        '<span aria-hidden="true">&times;</span>',
+                        "</button>",
+                        '<strong>Oops!</strong> An error occurred while sending your message. Please try again or email me directly at <a class="alert-link" href="mailto:a.jeffredo2@gmail.com">a.jeffredo2@gmail.com</a>.',
+                        "</div>"
+                    ]
+                        .join("")
+                        .replace(/\s\s+/g, "");
+
+                    $cardContent.append(errorAlert);
+
+                    $contactForm.trigger("reset");
+
+                    $formSubmitButton.attr("disabled", true);
+                }
+            );
+        }
+    });
+
+    var inputs = document.querySelectorAll("input, select, textarea, select");
+    inputs.forEach(function(input) {
+        var $input = $(input);
+        var $formGroup = $input.parents(".form-group");
+        var $feedback = $formGroup.find(".form-control-feedback");
+
+        input.addEventListener("input", function() {
+            if (input.validity.valid) {
+                $formGroup.removeClass("has-danger");
+            } else {
+                $formGroup.addClass("has-danger");
+                if (input.validity.typeMismatch && $input.attr("name") == "email") {
+                    $feedback.html("Please provide a valid email");
+                }
+                if (input.validity.valueMissing && $input.attr("name") == "email") {
+                    $feedback.html("Email is required");
+                }
+            }
+
+            if ($contactForm[0].checkValidity()) {
+                $formSubmitButton.removeAttr("disabled");
+            } else {
+                $formSubmitButton.attr("disabled", true);
+            }
+        });
+    });
+});
---
+++ webpack.config.js
@@ -0,0 +1,105 @@
+const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
+const CleanWebpackPlugin = require("clean-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
+const PurifyCSSPlugin = require("purifycss-webpack");
+const glob = require("glob");
+
+module.exports = {
+    entry: "./site.js",
+    context: path.resolve(__dirname, "root/src"),
+    output: {
+        filename: "site.[contentHash].js",
+        path: path.resolve(__dirname, "root/jekyll")
+    },
+    mode: "production",
+    devtool: "source-map",
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /(node_modules)/,
+                use: {
+                    loader: "babel-loader",
+                    options: {
+                        presets: ["@babel/preset-env"],
+                        plugins: ["@babel/plugin-proposal-class-properties"]
+                    }
+                }
+            },
+            {
+                test: /\.scss$/,
+                use: ["style-loader", MiniCssExtractPlugin.loader, "css-loader", "postcss-loader", "sass-loader"]
+            },
+            {
+                test: /\.(jpe?g|png)$/i,
+                loader: "responsive-loader",
+                options: {
+                    sizes: [500, 750, 1000],
+                    adapter: require("responsive-loader/sharp"),
+                    name: "assets/images/[name]-[hash]-[width].[ext]",
+                    placeholder: true
+                }
+            }
+            // {
+            //     test: /\.html$/,
+            //     use: {
+            //         loader: "html-loader",
+            //         options: {
+            //             interpolate: true
+            //         }
+            //     }
+            // }
+        ]
+    },
+    plugins: [
+        new CleanWebpackPlugin(["root/jekyll/*.js", "root/jekyll/*.css", "root/jekyll/*.map"], {}),
+        new MiniCssExtractPlugin({
+            filename: "site.[contentHash].min.css"
+        }),
+        // new PurifyCSSPlugin({
+        //     paths: glob.sync(path.join(__dirname, "root/jekyll/**/*.html"))
+        // }),
+        new HtmlWebpackPlugin({
+            inject: true,
+            template: "./templates/default-layout.tmpl.html",
+            filename: path.resolve(__dirname, "root/jekyll/_layouts/default.html")
+        }),
+        new HtmlWebpackPlugin({
+            inject: true,
+            template: "./templates/blog-layout.tmpl.html",
+            filename: path.resolve(__dirname, "root/jekyll/_layouts/blog.html")
+        }),
+        new HtmlWebpackPlugin({
+            inject: true,
+            template: "./templates/home-layout.tmpl.html",
+            filename: path.resolve(__dirname, "root/jekyll/_layouts/home.html")
+        }),
+        new HtmlWebpackPlugin({
+            inject: false,
+            template: "./templates/images-metadata.tmpl.js",
+            filename: path.resolve(__dirname, "root/jekyll/_data/images-metadata.json")
+        }),
+        new HtmlWebpackPlugin({
+            inject: false,
+            template: "./templates/srcset.tmpl.js",
+            filename: path.resolve(__dirname, "root/jekyll/_includes/img_srcset.html")
+        })
+    ],
+    optimization: {
+        minimizer: [
+            new OptimizeCSSAssetsPlugin({}),
+            new UglifyJsPlugin({
+                parallel: true,
+                sourceMap: true,
+                uglifyOptions: {
+                    compress: {
+                        inline: false
+                    }
+                }
+            })
+        ]
+    }
+};
